cf48cb5da8d8731cd1f59b9a6772084801e9e85d
Douglas Duarte <douglasduarte.eng@gmail.com>
2025-09-12 16:32:09 -0300

    refactor: remove unused CreateMealController import from listMeals function

--- src/functions/listMeals.ts
+++ src/functions/listMeals.ts
@@ -2,7 +2,6 @@ import { APIGatewayProxyEventV2 } from "aws-lambda";
 import { parseResponse } from "../utils/parseResponse";
 import { parseProtectedEvent } from "../utils/parseProtectedEvent";
 import { unauthorized } from "../utils/http";
-import { CreateMealController } from "../controllers/CreateMealController";
 import { ListMealsController } from "../controllers/ListMealsController";
 
 export async function handler(event: APIGatewayProxyEventV2) {
